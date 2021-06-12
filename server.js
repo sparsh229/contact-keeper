2c162d6ff282f336528d0761eea136e28fc88ddb
sparsh229 <sparsh.gupta229@gmail.com>
2021-06-13 01:15:00 +0530

    production build done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 //connect database
@@ -10,12 +11,19 @@ app.use(cors());
 //inti middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.json({ msg: "Welcome to contact keeper API" }));
-
 //define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 8080;
 
