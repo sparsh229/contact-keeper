600272d6c110bc96398e4a1a1e50d9d7780e6dc1
sparsh229 <sparsh.gupta229@gmail.com>
2021-06-12 15:19:06 +0530

    add animation

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -1,6 +1,7 @@
-import React, { Fragment, useContext } from "react";
+import React, { useContext } from "react";
 import ContactItem from "./ContactItem";
 import ContactContext from "../../context/contact/contactContext";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Grid } from "@material-ui/core";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
@@ -9,19 +10,25 @@ const Contacts = () => {
     <img src="./no-contact.svg" alt="no contact" />;
   }
   return (
-    <Grid container spacing={2}>
-      {filtered !== null
-        ? filtered.map((contact) => (
-            <Grid item xs={12} sm={6}>
-              <ContactItem key={contact.id} contact={contact} />
-            </Grid>
-          ))
-        : contacts.map((contact) => (
-            <Grid item xs={12} sm={6}>
-              <ContactItem key={contact.id} contact={contact} />
-            </Grid>
-          ))}
-    </Grid>
+    <TransitionGroup>
+      <Grid container spacing={2}>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} classNames="item" timeout={500}>
+                <Grid item xs={12} sm={6}>
+                  <ContactItem contact={contact} />
+                </Grid>
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} classNames="item" timeout={500}>
+                <Grid item xs={12} sm={6}>
+                  <ContactItem contact={contact} />
+                </Grid>
+              </CSSTransition>
+            ))}
+      </Grid>
+    </TransitionGroup>
   );
 };
 
